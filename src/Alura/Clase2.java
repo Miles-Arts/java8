038d05eece70be99892af1aefaa6127c36fc4b1e
Milton Figueredo Miles <gratno@gmail.com>
2023-02-16 23:25:18 -0500

    ForEach Java 8 y sort Java 8

--- src/Alura/Clase2.java
+++ src/Alura/Clase2.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Clase2 {
 
@@ -16,13 +17,23 @@ public class Clase2 {
         cursos.add("Curso de Geometría del espacio");
         cursos.add("Curso de Historia Universal");
 
-        Collections.sort(cursos, new CompararClase2());
+        //Collections.sort(cursos, new CompararClase2());
+        cursos.sort(new CompararClase2());
+        cursos.forEach(new ConsumidorClase2());
 
-        for (String curso : cursos) {
+       /* for (String curso : cursos) {
 
             System.out.println(curso);
 
-        }
+        }*/
+    }
+}
+
+class ConsumidorClase2 implements Consumer<String> {
+
+    @Override
+    public void accept(String s) {
+        System.out.println(s.length());
     }
 }
 
