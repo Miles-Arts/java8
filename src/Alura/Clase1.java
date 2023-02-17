61bd064a9390a03ae01e52151bd92134ec059706
Milton Figueredo Miles <gratno@gmail.com>
2023-02-16 22:20:28 -0500

    Java 8

---
+++ src/Alura/Clase1.java
@@ -0,0 +1,18 @@
+package Alura;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Clase1 {
+
+    public static void main(String[] args) {
+
+        List<String> cursos = new ArrayList<>();
+
+        cursos.add("cusro de Física");
+        cursos.add("cusro de Java");
+        cursos.add("cusro de Geometría del espaio");
+        cursos.add("cusro de Historia Universal");
+
+    }
+}
