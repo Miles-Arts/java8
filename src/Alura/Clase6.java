c91f7456f96cd5de10e66aba6c8723826e16719e
Milton Figueredo Miles <gratno@gmail.com>
2023-02-18 20:51:42 -0500

    Reference method

---
+++ src/Alura/Clase6.java
@@ -0,0 +1,29 @@
+package Alura;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Clase6 {
+
+    public static void main(String[] args) {
+
+        List<String> cursos = new ArrayList<>();
+
+        cursos.add("Curso de Física");
+        cursos.add("Curso de Java 8");
+        cursos.add("Curso de Geometría del espacio");
+        cursos.add("Curso de Historia Universal");
+
+        //Function<String, Integer> funcion = x -> x.length();
+
+        //cursos.sort(Comparator.comparing(funcion));
+        cursos.sort(Comparator.comparing(String::length));
+
+
+        cursos.forEach(System.out::println);
+    }
+}
+
+
+
